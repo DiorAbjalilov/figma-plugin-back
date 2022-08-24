5f70d5a036681f71a14bc5d0363d0de69f5e3375
DiorAbjalillov <abjalilovdiyor@gmail.com>
2022-08-24 12:09:06 +0500

    sdas

--- src/index.js
+++ src/index.js
@@ -91,12 +91,12 @@ const getFiles = async (search = "", filter = "", page = 0, pageSize = 100) => {
 //     cb(null, "uploadIcon" + "-" + uniqueSuffix);
 //   },
 // });
-// const upload = multer({ dest: "/resources/arrows/bulk/" });
+const upload = multer({ dest: "/resources/arrows/bulk/" });
 // post upload icon
-app.post("/upload", async (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     // const { folder, category } = req.body;
-    res.send({ message: "file upload" });
+    res.send({ message: "file upload", file: req.file, body: req.body });
   } catch (error) {
     res.send(error);
   }
